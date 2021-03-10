554dca0090739718f18f844eb3cf981b69fc4233
ArielColqueH <arielcolque444@gmail.com>
2021-03-09 20:58:06 -0400

    sign in with firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_ap/providers/auth.dart';
 import 'package:shop_ap/screens/auth_screen.dart';
 import 'package:shop_ap/screens/edit_product_screen.dart';
 import 'package:shop_ap/screens/user_products_screen.dart';
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
         ChangeNotifierProvider.value(
           value: Products(),
         ),
