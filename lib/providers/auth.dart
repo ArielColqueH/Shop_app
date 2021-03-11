a38eab02e53b2f8bb44a824421e611e0a3b7bdfc
ArielColqueH <arielcolque444@gmail.com>
2021-03-11 12:27:48 -0400

    sign in done

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,9 +7,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAkEgO_gNrZSccBAXszmW2fR6MfcNSSzC4';
+  Future<void> _authenticate(String email, String password,String urlSegment)async{
+    final url =
+//      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=xxx';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAkEgO_gNrZSccBAXszmW2fR6MfcNSSzC4';
     final response = await http.post(
       url,
       body: json.encode(
@@ -22,4 +23,14 @@ class Auth with ChangeNotifier {
     );
     print(json.decode(response.body));
   }
+  Future<void> signup(String email, String password) async {
+//  return _authenticate(email, password, 'signupNewUser');
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+//  return _authenticate(email, password, 'verifyPassword');
+    return _authenticate(email, password, 'signInWithPassword');
+  }
 }
+
